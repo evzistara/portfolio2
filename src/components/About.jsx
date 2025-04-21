f0e607a997d6264dfc620fd1905aacb592f3fc65
evizstara <evzi.stara@gmail.com>
2025-04-21 18:33:01 +0200

    Update About me text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,17 +11,17 @@ function About() {
       <div>
         <h2 className="text-3xl my-4 capitalize text-primary">About me</h2>
         <p className="mb-4 text-xl">
-          Hi! I'm Evzi — a junior Front-end developer based in <span className="font-bold text-primary">Oslo, Norway</span>.
+          Hi! I am Evzi — a junior Front-end developer based in <span className="font-bold text-primary">Oslo, Norway</span>.
         </p>
         <p className="mb-4">
-          I'm currently studying a<span className="font-bold text-primary"> year-long IT program at OsloMet</span> . And on the side I’m
+          I'm currently studying a<span className="font-bold text-primary"> year-long IT program at OsloMet</span> . And on the side I am
           diving deeper into web development and modern technologies.
         </p>
         <p className="mb-4">
           Curious by nature, I love asking questions, exploring new ideas, and
           figuring out how things work. I have a master’s degree in Economics
           and Marketing, and I worked as an <span className="font-bold text-primary">SEO & SEM
-          Specialist</span> in various marketing agencies. I love findining solutions to problems and make things better.
+          Specialist for about 4 years</span> in various marketing agencies. I love findining solutions to problems and make things better.
         </p>
         <p>
           Outside of coding, you’ll find me reading fantasy books, watching anime (Naruto, currently), doing calisthenics, or hanging out with my dog Bonnie.
