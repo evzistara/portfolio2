18f6c078b64b9be78d006f67946181fd922238a6
Evzi Stara <evzi.stara@gmail.com>
2025-10-30 09:22:58 +0100

    Move Skill inside Skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,40 +1,72 @@
 import data from "../data/skillData";
-import Skill from './Skill';
 
-function Skills(){
+export default function Skills() {
+  return (
+    <section id="skills" className="py-32 px-10 md:py-40 md:px-24">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-3xl mx-auto">
+          <h2 className="text-3xl md:text-4xl font-bold text-foreground mb-6 text-balance">
+            My Skills
+          </h2>
 
-    const skillsF = data.frontend.map((skill) => {
-        return <Skill key={skill.id} skill={skill} />;
-      });
-    
-      const skillsB = data.backend.map((skill) => {
-        return <Skill key={skill.id} skill={skill} />;
-      });
-    
-      const tools = data.tools.map((skill) => {
-        return <Skill key={skill.id} skill={skill} />;
-      });
-    
-    return(
-        <div className="lg:w-5xl lg:mx-auto">
-        <h2 className="text-3xl my-4 capitalize text-primary">My skills</h2>
-        <div className="my-10">
-          <h3 className="text-xl mb-2 text-text uppercase">Front-end</h3>
-          <div className="flex flex-wrap gap-5">
-          {skillsF}</div>
+          <div className="my-10">
+            <h3 className="text-xl mb-2 text-foreground">Front-end</h3>
+            <div className="flex flex-wrap gap-4 py-2 mb-1">
+              {data.frontend.map((skill) => (
+                <div
+                  key={skill.id}
+                  className="flex gap-2 bg-white rounded-md px-4 py-2 items-center mb-1 shadow-md shadow-primary/30 hover:shadow-lg hover:shadow-primary/50 transition-all duration-300 ease-in-out"
+                >
+                  <img
+                    className="h-10 w-10"
+                    src={skill.icon}
+                    alt={`Icon of ${skill.name}`}
+                  />
+                  <p>{skill.name}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="my-10">
+            <h3 className="text-xl mb-2  text-foreground">Back-end</h3>
+            <div className="flex flex-wrap gap-4 py-2 mb-1">
+              {data.backend.map((skill) => (
+                <div
+                  key={skill.id}
+                  className="flex gap-2 bg-white rounded-md px-4 py-2 items-center mb-1 shadow-md shadow-primary/30 hover:shadow-lg hover:shadow-primary/50 transition-all duration-300 ease-in-out"
+                >
+                  <img
+                    className="h-10 w-10"
+                    src={skill.icon}
+                    alt={`Icon of ${skill.name}`}
+                  />
+                  <p>{skill.name}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="my-10">
+            <h3 className="text-xl mb-2 text-foreground">Tools</h3>
+            <div className="flex gap-4 flex-wrap py-2 mb-1">
+              {data.tools.map((skill) => (
+                <div
+                  key={skill.id}
+                  className="flex gap-2 bg-white rounded-md px-4 py-2 items-center mb-1 shadow-md shadow-primary/30 hover:shadow-lg hover:shadow-primary/50 transition-all duration-300 ease-in-out"
+                >
+                  <img
+                    className="h-10 w-10"
+                    src={skill.icon}
+                    alt={`Icon of ${skill.name}`}
+                  />
+                  <p>{skill.name}</p>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-        <div className="my-10">
-          <h3 className="text-xl mb-2 uppercase text-primary">Back-end</h3>
-          <div className="flex flex-wrap gap-5">
-          {skillsB}</div>
-        </div>
-        <div className="my-10">
-          <h3 className="text-xl mb-2 uppercase text-primary">Tools</h3>
-          <div className="flex flex-wrap gap-5">
-          {tools}</div>
-        </div>
-       </div>
-    )
+      </div>
+    </section>
+  );
 }
-
-export default Skills;
