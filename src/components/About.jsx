ff50febb8933f72fa34ba6bf9c7e0c6632d576fa
Evzi Stara <evzi.stara@gmail.com>
2025-10-30 08:49:00 +0100

    Add new pictures, change text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,33 +1,46 @@
-import EvziS from "../assets/EvziS.jpg";
+import Evzi from "../assets/Evzi.jpg";
+import Bonnie from "../assets/EvziBonnie.jpeg";
+import EvziSport from "../assets/EvziSport.jpg";
 
 function About() {
   return (
-    <div className="flex flex-col items-center justify-center gap-5 md:flex-row md:gap-10 lg:w-5xl lg:mx-auto">
-      <img
-        className="h-100 rounded-xl shadow-gray-600 shadow-md block"
-        src={EvziS}
-        alt="Portrait of Evzi"
-      />
-      <div>
-        <h2 className="text-3xl my-4 capitalize text-primary">About me</h2>
-        <p className="mb-4 text-xl">
-          Hi! I am Evzi — a junior Front-end developer based in <span className="font-bold text-primary">Oslo, Norway</span>.
-        </p>
-        <p className="mb-4">
-          I'm currently studying a<span className="font-bold text-primary"> year-long IT program at OsloMet</span> . And on the side I am
-          diving deeper into web development and modern technologies.
-        </p>
-        <p className="mb-4">
-          Curious by nature, I love asking questions, exploring new ideas, and
-          figuring out how things work. I have a master’s degree in Economics
-          and Marketing, and I worked as an <span className="font-bold text-primary">SEO & SEM
-          Specialist for about 4 years</span> in various marketing agencies. I love findining solutions to problems and make things better.
-        </p>
-        <p>
-          Outside of coding, you’ll find me reading fantasy books, watching anime (Naruto, currently), doing calisthenics, or hanging out with my dog Bonnie.
-        </p>
+    <section id="about" className="py-32 px-10 md:py-40 md:px-24 bg-muted">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-3xl mx-auto">
+          <h2 className="text-3xl md:text-4xl font-bold text-foreground mb-6 text-balance">
+            About Me
+          </h2>
+          <div>
+            <p className="mb-4">
+              I am a Frontend developer based in Oslo, Norway. I have a master’s
+              degree in Economics and Marketing, one year IT study program from
+              OsloMet and a Frontend developer course from Scrimba. I worked as
+              an SEO & SEM Specialist for about 4 years in various marketing
+              agencies. I love findining solutions to problems and make things
+              better.
+            </p>
+            <p className="mb-6">
+              Curious by nature, I love asking questions, exploring new ideas,
+              and figuring out how things work. Outside of coding, you’ll find
+              me reading fantasy books, watching anime (Naruto, currently),
+              doing calisthenics, or hanging out with my dog Bonnie.
+            </p>
+          </div>
+          <div className="flex flex-col gap-4 items-center md:flex-row">
+            <div className="border border-primary rounded-md w-xs h-xs flex items-center justify-center">
+              <img className="w-xs h-xs rounded-md" src={Evzi} alt="" />
+            </div>
+            <div className="border border-primary rounded-md w-xs h-xs flex items-center justify-center">
+              <img className="w-xs h-xs object-cover rounded-md" src={EvziSport} alt="" />
+            </div> 
+            <div className="border border-primary rounded-md w-xs h-xs flex items-center justify-center">
+              <img className="w-xs h-xs object-cover rounded-md" src={Bonnie} alt="" />
+            </div>
+            
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
