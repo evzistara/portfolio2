5de3cfedcc843c6ce810002195c69d92534eddd8
Evzi Stara <evzi.stara@gmail.com>
2025-10-30 16:36:10 +0100

    Change information

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,42 +1,33 @@
-import Oslomet from "../assets/experience/oslomet.png";
-import Vsem from "../assets/experience/vsem.png";
-import Vse from "../assets/experience/vse.jpg";
-import Publicis from "../assets/experience/publicis.jpg";
-import RepriseDigital from "../assets/experience/reprise_digital.jpg";
-import Idium from "../assets/experience/idium.jpg";
-
-
 export default {
     "university": [
       {
         "id": 1,
-        "name": "OsloMet",
-        "location": "Oslo, Norway",
-        "period": "08/2024 - 06/2025",
-        "degree": "1 year program",
-        "program": "IT, 60 p.",
-        "other": "Inclusive web design, Programing, Interaction design, IoT, Databases, Web programming",
-        "logo": Oslomet
+        "name": "Frontend Development Career Path",
+        "school": "Online course, Scrimba",
+        "period": "06/2025 - 10/2025",
+        "info": "Completed comprehensive coursework in modern frontend framworks and best practices"
+     
+
+     
       },
       {
         "id": 2,
-        "name": "University of Economy and Management",
-        "location": "Prague, Czechia",
-        "period": "09/2018 - 06/2020",
-        "degree": "Master's degree",
-        "program": "Economy and management",
-        "other": "Marketing and advertising",
-        "logo": Vsem
+        "name": "1 year IT program OsloMet",
+        "school" : "OsloMet",
+        "period": "08/2024 - 06/2025",
+        "info": "I studied following subjects: Inclusive web design, Programing, Interaction design, IoT, Databases, Web programming",
+      
       },
       {
         "id": 3,
-        "name": "University of Economics",
-        "location": "Prague, Czechia",
-        "period": "09/2015 - 06/2018",
-        "degree": "Bachelor's degree",
-        "program": "Economy & Business law",
-        "logo": Vse
-      }
+        "name": "Master's degree in Economy and Management",
+        "school": "University of Economy and Management",
+        "period": "09/2018 - 06/2020",
+        "info": "I specialized in marketing and advertisment and got to learn about both offline and offline marketing"
+    
+  
+      },
+  
     ],
     "work": [
       {
@@ -44,29 +35,25 @@ export default {
         "title": "Search Executive",
         "company": "Publicis Norway",
         "period": "05/2023 - 06/2024",
-        "clients": "Clients: Ice B2C, Ice B2B, Nice Mobil & Bertel og Steen",
-        "tasks": [
-          "Day-to-day paid search accounts optimization, testing",
-          "Involved in various projects: use of GPT for keyword analysis; use of first party data in search accounts; automatization in paid search with use of scripts and third party tools"
-        ],
-        "logo": Publicis
+        "tasks": "I worked with Google paid search for clients as Ice and Bertel og Steen. Apart from daily optimization, I was involved in various technical projects: use of GPT for keyword analysis; use of first party data in search accounts; automatization in paid search with use of scripts and third party tools"
+        ,
+    
       },
       {
         "id": 2,
         "title": "SEO specialist",
         "company": "Reprise Digital",
         "period": "01/2023 - 05/2023",
-        "clients": "Clients: Fagmøbler, Torshov Sport",
-        "tasks":"Creating on-page SEO strategy, Keyword analysis, competitor analysis",
-        "logo": RepriseDigital
+        "tasks":"In this role I was mainly responsible for creating on-page SEO strategy for different clients. I was also doing keyword, competitor and content analysis.",
+      
       },
       {
         "id": 3,
         "title": "SEO consultant",
         "company": "Idium 1881",
         "period": "04/2022 - 12/2022",
-        "tasks": "On-page and technical SEO for different clients, SEO analysis, keywords analysis, competitor analysis, Cooperation with content writers, web developers, Member of social committee and internal marketing team",
-        "logo": Idium
+        "tasks": "I was doing on-page and technical SEO for different clients, together with SEO analysis, keywords analysis, competitor analysis. I cooperated with content writers, web developers. I was also member of social committee and internal marketing team",
+   
       }
     ]
   }
